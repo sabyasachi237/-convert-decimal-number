ee76da0fe4fba4a8f003737316dfadc46af772af
sabyasachi sen <sabyasachi237@gmail.com>
2022-08-02 11:31:09 +0530

    first commit

---
+++ decimal.py
@@ -0,0 +1,18 @@
+decimal = int(input('enter a number here: '))
+
+
+
+print("the conversion of decimal number", decimal, "is: ")
+
+
+
+print(bin(decimal), "in binary")
+
+
+
+print(oct(decimal), "in octal")
+
+
+
+
+print(hex(decimal), "in hexadecimal")
